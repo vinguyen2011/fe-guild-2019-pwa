91a79f4728e1e9c5d09fb109c88a20de82b071b3
Vi (T.T.V.) Nguyen <vi.nguyen2@ing.nl>
2019-01-30 11:12:01 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,32 @@
+const CACHE_STATIC_NAME = 'static';
+const URLS_TO_PRECACHE = [
+    '/',
+    'sw.js',
+    'index.html',
+    'src/js/app.js',
+    'src/js/feed.js',
+    'src/lib/material.min.js',
+    'src/css/app.css',
+    'src/css/feed.css',
+    'src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    // 'https://code.getmdl.io/1.3.0/material.indigo-deep_orange.min.css"'
+];
+
 self.addEventListener('install', event => {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(self.skipWaiting());
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(cache => {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll(URLS_TO_PRECACHE);
+            })
+            .then(() => {
+                console.log('[ServiceWorker] Skip waiting on install');
+                return self.skipWaiting();
+            })
+    );
 });
 
 self.addEventListener('activate', event => {
@@ -11,10 +37,15 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   console.log('[Service Worker] Fetching something ....', event);
 
-  // This fixes a weird bug in Chrome when you open the Developer Tools
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-    return;
-  }
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log(response);
+                    return response;
+                }
 
-  event.respondWith(fetch(event.request));
+                return fetch(event.request);
+            })
+    );
 });
