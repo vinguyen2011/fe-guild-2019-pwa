fb1047743b428be0566ad78798086088551981fa
Vi (T.T.V.) Nguyen <vi.nguyen2@ing.nl>
2019-01-30 11:17:03 +0100

    Add more logic

--- fe-guild-2019-pwa/src/js/app.js
+++ fe-guild-2019-pwa/src/js/app.js
@@ -38,10 +38,10 @@ const manifest = {
             'sizes': '512x512'
         }
     ],
-    //'start_url': '/index.html',
-    //"scope": ".",
+    // 'start_url': '/index.html',
+    // "scope": ".",
     'display': 'standalone',
-    'orientation': 'portrait-primary',
+    'orientation': 'portrait',
     'background_color': '#fff',
     'theme_color': '#3f51b5',
     'description': 'Take selfies PWA style.',
@@ -58,6 +58,7 @@ window.addEventListener('load', () => {
         baseUrl = `${baseUrl}/`;
     }
 
+    // manifest
     manifest['start_url'] = `${baseUrl}index.html`;
 
     manifest.icons.forEach(icon => {
@@ -68,4 +69,34 @@ window.addEventListener('load', () => {
     const blob = new Blob([stringManifest], {type: 'application/json'});
     const manifestURL = URL.createObjectURL(blob);
     document.querySelector('#manifestPlaceholder').setAttribute('href', manifestURL);
+
+
+    // service worker
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register(`${baseUrl}sw.js`)
+        .then( registration => {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(err => {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    }
+});
+
+
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', event => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  event.preventDefault();
+
+  console.log('beforeinstallprompt fired');
+
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event;
+
+  return false;
 });
---
+++ fe-guild-2019-pwa/sw.js
@@ -0,0 +1,73 @@
+const CACHE_STATIC_NAME = 'static';
+const URLS_TO_PRECACHE = [
+    '/',
+    'sw.js',
+    'index.html',
+    'src/js/app.js',
+    'src/js/feed.js',
+    'src/lib/material.min.js',
+    'src/css/app.css',
+    'src/css/feed.css',
+    'src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    // 'https://code.getmdl.io/1.3.0/material.indigo-deep_orange.min.css"'
+];
+
+self.addEventListener('install', event => {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(cache => {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll(URLS_TO_PRECACHE);
+            })
+            .then(() => {
+                console.log('[ServiceWorker] Skip waiting on install');
+                return self.skipWaiting();
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activating Service Worker ...', event);
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  console.log('[Service Worker] Fetching something ....', event);
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log(response);
+                    return response;
+                }
+
+                // Clone the request - a request is a stream and can be only consumed once
+                const requestToCache = event.request.clone();
+
+                // Try to make the original HTTP request as intended
+                return fetch(requestToCache)
+                    .then(response => {
+                        // If request fails or server responds with an error code, return that error immediately
+                        if (!response || response.status !== 200) {
+                            return response;
+                        }
+
+                        // Again clone the response because you need to add it into the cache and because it's used
+                        // for the final return response
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_DYNAMIC_NAME)
+                            .then(cache => {
+                                cache.put(requestToCache, responseToCache);
+                            });
+
+                        return response;
+                    })
+            })
+            .catch(error => console.log('[Service Worker] Dynamic cache error.', error))
+    );
+});
